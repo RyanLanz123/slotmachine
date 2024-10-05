27601712059c28810c3ba1b357bd0dd3d168f715
RyanLanz123 <ryanlanz6@gmail.com>
2024-10-05 11:17:47 +0100

    get bet amount

--- project.js
+++ project.js
@@ -26,5 +26,32 @@ const deposit = () => {
     }
 };
 
-const depositAmount = deposit();
-console.log(depositAmount);
+const getNumberOfLines = () => {
+    while (true){
+        const lines = prompt("Enter number of lines to bet (1-3): ");
+        const numberOfLines = parseFloat(lines);
+
+        if(isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3){
+            console.log("Invalid number of lines, try again.")
+        } else {
+            return numberOfLines;
+        }
+    }
+}
+
+const getBet = (balance, lines) => {
+    while (true){
+        const bet = prompt("Enter total bet per line: ");
+        const numberBet = parseFloat(bet);
+
+        if(isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines){
+            console.log("Invalid bet, try again.")
+        } else {
+            return numberBet;
+        }
+    }
+}
+
+let balance = deposit(); //let allows adjustment of variables
+const numberOfLines = getNumberOfLines(); //const can't be changed (constant)
+const bet = getBet(balance, numberOfLines);
