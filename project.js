d537e0107954763aa7cda845477a14f3eb9b51b2
RyanLanz123 <ryanlanz6@gmail.com>
2024-10-05 12:48:37 +0100

    print slot machine

--- project.js
+++ project.js
@@ -77,10 +77,11 @@ const spin = () => {
         }
     }
 
-    [A, B, C]
+
     
-    const reels = [[], [], []]; //represents  column in slots
-    for (let i = 0; i < COLS; i++){
+    const reels = []; 
+    for (let i = 0; i < COLS; i++){ //loops reels
+        reels.push([]);
         const reelSymbols = [...symbols];
         for (let j = 0; j < ROWS; j++){
             const randomIndex = Math.floor(Math.random() * reelSymbols.length)
@@ -89,10 +90,37 @@ const spin = () => {
             reelSymbols.splice(randomIndex, 1);
         }
     }
+    return reels;
 };
 
-spin();
+const transpose = (reels) => {
+    const rows = [];
+    for(let i=0; i < ROWS; i++){
+        rows.push([]);
+        for (let j = 0; j < COLS; j++){
+            rows[i].push(reels[j][i])//accesses columns then returns rows
+        }
+    }
+
+    return rows
+}
+
+const printRows = (rows) => {
+    for (const row of rows) {
+        let rowString = "";
+        for(const [i, symbol] of row.entries()){
+            rowString += symbol //concactinate elements into the string
+            if (i != row.length -1){
+                rowString += " | "
+            }
+        }
+        console.log(rowString)
+    }
+}
 
 let balance = deposit(); //let allows adjustment of variables
 const numberOfLines = getNumberOfLines(); //const can't be changed (constant)
 const bet = getBet(balance, numberOfLines);
+const reels = spin();
+const rows = transpose(reels);
+printRows(rows);
