4cb44d62fcf5452da975228c48814c2014468ef1
RyanLanz123 <ryanlanz6@gmail.com>
2024-10-05 12:32:20 +0100

    added spin feature

--- project.js
+++ project.js
@@ -12,6 +12,23 @@
 
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+const SYMBOL_VALUES = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2,
+}
+
 
 const deposit = () => {
     while (true){
@@ -51,6 +68,30 @@ const getBet = (balance, lines) => {
         }
     }
 }
+
+const spin = () => {
+    const symbols = [];
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)){
+        for (let i = 0; i < count; i++){
+            symbols.push(symbol); //push(append) symbols into array
+        }
+    }
+
+    [A, B, C]
+    
+    const reels = [[], [], []]; //represents  column in slots
+    for (let i = 0; i < COLS; i++){
+        const reelSymbols = [...symbols];
+        for (let j = 0; j < ROWS; j++){
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length)
+            const selectedSymbol =  reelSymbols[randomIndex]
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1);
+        }
+    }
+};
+
+spin();
 
 let balance = deposit(); //let allows adjustment of variables
 const numberOfLines = getNumberOfLines(); //const can't be changed (constant)
